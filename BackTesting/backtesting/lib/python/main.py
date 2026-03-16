c91dd1da100dfb0e293289d987f3150370e3ae23
Harborg1 <caharborg@gmail.com>
2026-03-16 16:51:58 +0100

    Update main.py

--- BackTesting/backtesting/lib/python/main.py
+++ BackTesting/backtesting/lib/python/main.py
@@ -199,5 +199,8 @@ def get_available_tickers():
     return {"tickers": TICKER_UNIVERSE}
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    
+    port = int(os.environ.get("PORT", 8000))
+    
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
